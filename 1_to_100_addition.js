bfc9be8b345a89314ff074972fba079feab6d915
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-06 12:58:34 +0600

    1_to_100_addition

---
+++ 1_to_100_addition.js
@@ -0,0 +1,22 @@
+var n = 0;
+for (let index = 1; index <= 100; index++) {
+    n = n + index;
+    console.log(index);
+}
+
+console.log("summation of 1 to 100 number is: " + n)
+
+
+
+
+
+
+
+
+// var n = 0;
+// for (let index = 1; index <= 100; index++) {
+//     n = n + index;
+    
+// }
+
+// console.log(n);
