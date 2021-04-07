7ab9f16942dddc55c065c4ce2263342e9e5b41eb
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-07 18:51:55 +0600

    Star_structure_print_use_of_for_loop

---
+++ Star_structure_print_use_of_for_loop.js
@@ -0,0 +1,11 @@
+
+
+let string = "";
+for (let i = 1; i <=5 ; i++) {
+    for (let j = 0; j < 5; j++) {
+        string = string + "*";
+    }
+    string = string + "\n";
+}
+
+console.log(string);
