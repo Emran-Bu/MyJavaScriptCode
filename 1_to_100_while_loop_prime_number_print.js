be2442262d3d8430486c2a7283567cc8fd8d262c
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-07 13:19:07 +0600

    1_to_100_while_loop_prime_number_print

---
+++ 1_to_100_while_loop_prime_number_print.js
@@ -0,0 +1,18 @@
+
+var i = 1;
+while (i <= 100) {
+    var check = "prime";
+        var j = 2;
+        while (j < i) {
+            if (i % j == 0) {
+                check = "not prime"
+                break;
+            }
+            j++;
+        }
+
+        if (check == "prime") {
+            console.log(i);
+        }
+    i++;
+}
