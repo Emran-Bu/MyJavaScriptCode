a80b59330907cfe1b1d8cc0267e2394b7b1cf0d3
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-07 18:51:26 +0600

    sum_all_cubic_value_form_15-75

---
+++ sum_all_cubic_value_form_15-75.js
@@ -0,0 +1,14 @@
+
+for (let index = 15; index <= 75; index++) {
+    var h =(index * index * index);
+    console.log(index * index * index);
+}
+
+var sum = 0;
+
+for (let j = 15; j <= 75; j++) {
+       sum = sum + j ** 3;
+}
+
+console.log("summation all cubic value of 15 to 75 number is: " + sum)
+
