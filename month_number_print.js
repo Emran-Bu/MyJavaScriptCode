b78b96c94df539c1b3aaf95cf712e8a51a2181a3
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-10 18:09:37 +0600

    month number pritn

--- month_number_print.js
+++ month_number_print.js
@@ -1,1 +1,53 @@
-var month 
+var month = "july";
+
+if (month == "january") {
+    console.log("1")
+}
+
+else if (month == "february") {
+    console.log("2")
+}
+
+else if (month == "march") {
+    console.log("3")
+}
+
+else if (month == "april") {
+    console.log("4")
+}
+
+else if (month == "may") {
+    console.log("5")
+}
+
+else if (month == "june") {
+    console.log("6")
+}
+
+else if (month == "july") {
+    console.log("7")
+}
+
+else if (month == "august") {
+    console.log("8")
+}
+
+else if (month == "september") {
+    console.log("9")
+}
+
+else if (month == "october") {
+    console.log("10")
+}
+
+else if (month == "november") {
+    console.log("11")
+}
+
+else if(month == "december") {
+    console.log("12")
+}
+
+else{
+    console.log("month not found")
+}
