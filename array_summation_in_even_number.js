bbd4ee66029aa78121115d874b06bcc716d24b51
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-11 12:34:15 +0600

    f

---
+++ array_summation_in_even_number.js
@@ -0,0 +1,11 @@
+
+var sum = 0;
+var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+
+for (let i = 0; i < arr.length; i++) {
+  if (arr[i]%2==0) {
+      console.log(arr[i])
+      sum = sum + arr[i]
+  }
+}
+console.log(sum);
