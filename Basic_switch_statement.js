f6e0e68a0c4552b13d0626de1b4964116e8413d2
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-12 08:34:30 +0600

    Basic_switch_satement

---
+++ Basic_switch_statement.js
@@ -0,0 +1,10 @@
+var a =10;
+var b =20;
+switch (true) {
+    case (a>b):
+        console.log("a is greater then b")
+        break;
+
+    default:console.log("b is greater then a")
+        break;
+}
