6df4d1467f26884c0fe76c43ffaf4ca631f471ee
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-12 09:03:57 +0600

    if_date check_statement

---
+++ if_date_check_statement.js
@@ -0,0 +1,37 @@
+// if_date check_statement
+
+var date = new Date();
+
+var today = date.getDay();
+
+if (today == 0) {
+    console.log("Today is Sunday")
+}
+
+else if (today == 1) {
+    console.log("Today is Monday")
+}
+
+else if (today == 2) {
+    console.log("Today is Tuesday")
+}
+
+else if (today == 3) {
+    console.log("Today is wednesday")
+}
+
+else if (today == 4) {
+    console.log("Today is Thursday")
+}
+
+else if (today == 5) {
+    console.log("Today is Friday")
+}
+
+else if (today == 6) {
+    console.log("Today is Saturday")
+}
+
+else{
+    console.log("Date Not Found")
+}
