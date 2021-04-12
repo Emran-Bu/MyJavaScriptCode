164e2298091a508017c6a76e3e1e37d5175978ed
Freelancer-Emran <mdemranhasan809@gmail.com>
2021-04-12 10:04:30 +0600

    Ternary_operator

---
+++ Ternary_operator.js
@@ -0,0 +1,12 @@
+var n = 29;
+
+var result = n % 2 === 0 ? 'EVEN' : 'ODD';
+
+console.log(result)
+//
+var num1 = 10;
+var num2 = 20;
+
+var sum =num1 > num2 ? 'true' : 'false';
+
+console.log(sum);
